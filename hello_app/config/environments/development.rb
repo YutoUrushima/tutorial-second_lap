9a7497d08820885c06a4b512b4f427b0552787e4
YutoUrushima <yuto259233@gmail.com>
2022-01-28 14:21:12 +0900

    hello, world! & Routing

--- hello_app/config/environments/development.rb
+++ hello_app/config/environments/development.rb
@@ -56,6 +56,9 @@ Rails.application.configure do
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
+  # Cloud9 への接続を許可する
+  config.hosts.clear
+
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker

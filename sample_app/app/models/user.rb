0437dd1584b296f44ca37bb86313b424378922e7
Yuto Urushima <yuto259233@gmail.com>
2022-02-22 16:19:26 +0900

    10.1&10.2

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
               case_sensitive: false,
             }
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   # 渡されたハッシュ値を返す
   def User.digest(string)
